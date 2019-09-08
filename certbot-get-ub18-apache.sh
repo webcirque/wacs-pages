d4a0638272b59fcca8e8e0cf70b4007784f73267
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2019-09-08 12:04:01 +0800

    Create certbot-get-ub18-apache.sh

---
+++ certbot-get-ub18-apache.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get update
+apt-get install software-properties-common
+add-apt-repository universe
+add-apt-repository ppa:certbot/certbot
+apt-get update
+apt-get install certbot python-certbot-apache
