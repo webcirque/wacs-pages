eb6cc0964b2d17b0db5755fe77240a71c61729d9
Ediel Chedlyramber <15964383+EdChdX@users.noreply.github.com>
2020-06-12 21:53:10 +0800

    Update syncthing.sh

--- uis-ub18/syncthing.sh
+++ uis-ub18/syncthing.sh
@@ -11,7 +11,7 @@ echo "(for example, /home/user/syncthing): "
 read tmphmdir
 echo "Are you using Syncthing on a server?\n(1 for yes with LetsEncrypt / Certbot; 2 for yes with custom certification; 0 for no, which is default)"
 read tmpnum
-if ["$tmpnum" == "1"]; then
+if [ "$tmpnum" == "1" ]; then
   echo "Type your domain: "
   read tmpdomain
   echo "Setting up your Syncthing instance..."
@@ -19,7 +19,7 @@ if ["$tmpnum" == "1"]; then
   tmpcertpath="/etc/letsencrypt/live/$tmpdomain/"
   tmpcertpub="fullchain.pem"
   tmpcertpri="privkey.pem"
-elif ["$tmpnum" == "2"]; then
+elif [ "$tmpnum" == "2" ]; then
   echo "Type the absolute path to your certificate pair directory (with a / in the end)\nFor example, /tmp/folder/: "
   read tmpcertpath
   tmpcert=1
@@ -34,15 +34,15 @@ fi
 echo "Generating start script..."
 if [ ! -d "/usr/usi" ] ; then mkdir /usr/usi ; fi
 echo "#!/bin/bash">/usr/usi/syncthing.sh
-echo "export HOMEDIR=$tmphmdir">/usr/usi/syncthing.sh
-echo "GUIPORT=$tmpport">/usr/usi/syncthing.sh
+echo "HOMEDIR=$tmphmdir">>/usr/usi/syncthing.sh
+echo "GUIPORT=$tmpport">>/usr/usi/syncthing.sh
 if [ "$tmpcert" == "1" ] ; then
-  echo "if [ -d \"\$HOMEDIR/https-cert.pem\" ] ; then rm -f \"\$HOMEDIR/https-cert.pem\" ; fi">/usr/usi/syncthing.sh
-  echo "if [ -d \"\$HOMEDIR/https-key.pem\" ] ; then rm -f \"\$HOMEDIR/https-key.pem\" ; fi">/usr/usi/syncthing.sh
-  echo "cp \"$tmpcertpath$tmpcertpub\" \"\$HOMEDIR/https-cert.pem\">/usr/usi/syncthing.sh
-  echo "cp \"$tmpcertpath$tmpcertpri\" \"\$HOMEDIR/https-key.pem\">/usr/usi/syncthing.sh
+  echo "if [ -d \"\$HOMEDIR/https-cert.pem\" ] ; then rm -f \"\$HOMEDIR/https-cert.pem\" ; fi">>/usr/usi/syncthing.sh
+  echo "if [ -d \"\$HOMEDIR/https-key.pem\" ] ; then rm -f \"\$HOMEDIR/https-key.pem\" ; fi">>/usr/usi/syncthing.sh
+  echo "cp \"$tmpcertpath$tmpcertpub\" \"\$HOMEDIR/https-cert.pem\">>/usr/usi/syncthing.sh
+  echo "cp \"$tmpcertpath$tmpcertpri\" \"\$HOMEDIR/https-key.pem\">>/usr/usi/syncthing.sh
 fi
-echo "syncthing -gui-address=0.0.0.0:\$GUIPORT -home=\$HOME"
+echo "syncthing -gui-address=0.0.0.0:\$GUIPORT -home=\$HOME">>/usr/usi/syncthing.sh
 echo "Finished. Before continueing, you need to know..."
 echo "Syncthing does not have a service installed by default, but we provided a way to run Syncthing without trouble."
 echo "Just run Syncthing by running /usr/usi/syncthing.sh"
