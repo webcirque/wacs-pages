db374eadf8531f1f9ce8b0ccec9632c67b89126a
Ediel Chedlyramber <15964383+EdChdX@users.noreply.github.com>
2020-07-12 20:13:02 +0800

    Update speedtest.sh

--- uis-ub18/speedtest.sh
+++ uis-ub18/speedtest.sh
@@ -4,5 +4,6 @@ export INSTALL_KEY=379CE192D401AB61
 export DEB_DISTRO=$(lsb_release -sc)
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
 echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | tee /etc/apt/sources.list.d/speedtest.list
-apt-get update
-apt-get install speedtest
+apt update
+apt remove --purge -y speedtest-cli
+apt install speedtest -y
