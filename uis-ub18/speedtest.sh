f1f1b230bf979167c8c118f6a9a363cc1d5fd61f
Ediel Chedlyramber <15964383+EdChdX@users.noreply.github.com>
2020-08-18 12:25:16 +0800

    Update speedtest.sh

--- uis-ub18/speedtest.sh
+++ uis-ub18/speedtest.sh
@@ -3,7 +3,7 @@ apt-get install gnupg1 apt-transport-https dirmngr
 export INSTALL_KEY=379CE192D401AB61
 export DEB_DISTRO=$(lsb_release -sc)
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
-echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | tee /etc/apt/sources.list.d/speedtest.list
+echo "deb https://ookla.bintray.com/debian bionic main" | tee /etc/apt/sources.list.d/speedtest.list
 apt update
 apt remove --purge -y speedtest-cli
 apt install speedtest -y
