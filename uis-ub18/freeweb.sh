0a01a860c5e0f426b31bfb63b15dff9e7aecf7a9
Ediel Chedlyramber <edchdx@pm.me>
2020-12-25 18:46:14 +0000

    Update freeweb.sh

--- uis-ub18/freeweb.sh
+++ uis-ub18/freeweb.sh
@@ -4,12 +4,13 @@ echo Installing prequisites...
 apt install -y apt-transport-https software-properties-common
 echo Installing Tor and Nyx...
 apt install -y tor nyx
+read -p 'Do you have IPv6? (y for confirmation): ' getipv6
 if [ ! -e "tor/defaultTorrc" ] ; then
 	echo 'Configuring Tor for the first time...'
 	mv tor/torrc tor/defaultTorr
 	echo "ExcludeNodes {kp},{ir},{cu},{cn},{hk},{mo},{ru},{sy},{pk}" > tor/torrc
 	echo "StrictNodes 1" >> tor/torrc
-	read -p 'Do you have IPv6? (y for confirmation): ' getipv6
+	
 	if [ "$getipv6" == "y" ] ; then
 		echo "ClientUseIPv6 1" >> tor/torrc
 		echo "IPv6Exit 1" >> tor/torrc
@@ -19,5 +20,8 @@ echo Installing i2pd...
 add-apt-repository ppa:purplei2p/i2pd
 apt update
 apt install -y i2pd
+if [ ! "$getipv6" == "y" ] ; then
+	sed -i "s/ipv6 = false/ipv6 = true/g" i2pd/i2pd.conf
+fi
 echo Done.
 exit
