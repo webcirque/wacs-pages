9f1ba636b3cc1015714305cb88d461509d36018d
Ediel Chedlyramber <edchdx@pm.me>
2020-12-25 15:38:39 +0000

    Create caddy.sh

---
+++ uis-ub18/caddy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+echo You are about to install Caddy HTTP Server on your machine.
+echo Please make sure that you already have ROOT permissions.
+read tmp1
+echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" | tee -a /etc/apt/sources.list.d/caddy-fury.list
+apt update
+apt install -y caddy
+exit
