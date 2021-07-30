9d2b611ea426916e0155efb219586bc12cac4c2b
Ediel Chedlyramber <15964383+EdChdX@users.noreply.github.com>
2021-07-30 06:57:45 +0000

    Update speedtest.sh

--- debian/speedtest.sh
+++ debian/speedtest.sh
@@ -10,9 +10,13 @@ export INSTALL_KEY=379CE192D401AB61
 echo "Adding keys..."
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
 echo "Adding source repository..."
-echo "deb https://ookla.bintray.com/debian generic main" | tee /etc/apt/sources.list.d/speedtest.list
-echo "Updating repository..."
-apt update
+#echo "deb https://ookla.bintray.com/debian generic main" | tee /etc/apt/sources.list.d/speedtest.list
+#echo "Updating repository..."
+#apt update
+if [ -e "/etc/apt/sources.list.d/speedtest.list" ] ; then
+	rm /etc/apt/sources.list.d/speedtest.list
+fi
+bash <(curl -Ls https://install.speedtest.net/app/cli/install.deb.sh)
 echo "Removing speedtest-cli..."
 apt remove speedtest-cli -y
 echo "Installing..."
